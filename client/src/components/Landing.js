d90218f312249166ff8fda6c02acbd6501d70ed4
Linxi Xu <xulinxi@sas.upenn.edu>
2021-02-15 11:08:43 -0500

    Update browser tab icon

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,28 +1,30 @@
 import React from "react";
+import { SketchPicker} from 'react-color';
 import { Link } from "react-router-dom";
 import { Container, Button, Row, Col } from 'react-bootstrap';
 
 const Landing = () => {
   return (
-    <div className="text-center" style ={{paddingTop: "30vh"}}>
+    <div className="text-center" style ={{paddingTop: "35vh"}}>
             
             <div id="logo-container">
                 <div id="openVessel-logo">
-                    <img alt="OpenVessel Logo" src={process.env.PUBLIC_URL + '/img/OpenVessel Logo.png'} style={{width: '50%', height: '30%'}}></img>
+                    <img alt="OpenVessel Logo" src={process.env.PUBLIC_URL + '/img/OpenVessel Logo.png'} style={{width: '80%', height: '50%'}}></img>
                 </div>
             </div>
+
             <div>
-                  <Container>
-                    <Row className="justify-content-md-center">
-                      <Col xs lg="2">
-                        <Link to="/login"><Button>Login</Button></Link>
-                      </Col>
-                      <Col md="auto"></Col>
-                      <Col xs lg="2">
-                        <Link to="/register"><Button>Register</Button></Link>
-                      </Col>
-                    </Row>
-                  </Container>
+              <Container>
+                <Row className="justify-content-md-center">
+                  <Col xs lg="2">
+                    <Link to="/login"><Button>Login</Button></Link>
+                  </Col>
+                  <Col md="auto"></Col>
+                    <Col xs lg="2">
+                    <Link to="/register"><Button>Register</Button></Link>
+                  </Col>
+                </Row>
+              </Container>
             </div>
     </div>
 
