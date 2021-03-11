3267ccaf72be8a390f9716117292de018a5faeae
Linxi Xu <xulinxi@sas.upenn.edu>
2021-03-10 21:45:22 -0500

    Trials for uploading the image

--- client/src/components/Account.js
+++ client/src/components/Account.js
@@ -1,8 +1,8 @@
-import React, {useState} from "react";
-import { Link } from "react-router-dom";
-import { Button, Row, Col, Container } from 'react-bootstrap';
-// import React from 'react';
-import styled from 'styled-components';// Style the Button component
+// import React, {useState} from "react";
+// import { Link } from "react-router-dom";
+// import { Button, Row, Col, Container } from 'react-bootstrap';
+// // import React from 'react';
+// import styled from 'styled-components';// Style the Button component
 
 // import React from 'react';
 // import styled from 'components';
@@ -150,99 +150,171 @@ import styled from 'styled-components';// Style the Button component
 
 // -------------------------------------------------------
 
+// (most recent)
 // import React, { useState } from "react";
 // import "client/src/App.css";
-function Form(props) {
-    const [Username, setUsername] = useState("");
-    // const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+
+
+// import React, {useState} from "react";
+// import { Link } from "react-router-dom";
+// import { Button, Row, Col, Container } from 'react-bootstrap';
+// // import React from 'react';
+// import styled from 'styled-components';// Style the Button component
+// import ReactDOM from "react-dom"
+
+
+
+// function Form(props) {
+//     const [Username, setUsername] = useState("");
+//     // const [lastName, setLastName] = useState("");
+//     const [email, setEmail] = useState("");
+//     const [password, setPassword] = useState("");
     
-    const hiddenFileInput = React.useRef(null);
+//     // const hiddenFileInput = React.useRef(null);
     
-      // Programatically click the hidden file input element
-      // when the Button component is clicked
-    const handleClick = event => {
-        hiddenFileInput.current.click();
-      };  // Call a function (passed as a prop from the parent component)
-      // to handle the user-selected file 
-    const handleChange = event => {
-        const fileUploaded = event.target.files[0];
-        props.handleFile(fileUploaded);
-        };
+//     //   // Programatically click the hidden file input element
+//     //   // when the Button component is clicked
+//     // const handleClick = event => {
+//     //     hiddenFileInput.current.click();
+//     //   };  // Call a function (passed as a prop from the parent component)
+//     //   // to handle the user-selected file 
+//     // const handleChange = event => {
+//     //     const fileUploaded = event.target.files[0];
+//     //     props.handleFile(fileUploaded);
+//     //     };
+//     // ------------------------
+
+//     const handleImageUpload = e => {
+//         const uploadedImage = React.useRef(null);
+//         const imageUploader = React.useRef(null);
+
+//         const handleImageUpload = e => {
+//             const [file] = e.target.files;
+//             if (file) {
+//                 const reader = new FileReader();
+//                 const { current } = uploadedImage;
+//                 current.file = file;
+//                 reader.onload = e => {
+//                     current.src = e.target.result;
+//                 };
+//             reader.readAsDataURL(file);
+//             }
+//         }
+//     }
+
+
+
     
-    const Button = styled.button`
-       /* Insert your favorite CSS code to style a button */
-    `;
-    // const FileUploader = props => {  // Create a reference to the hidden file input element
+//     const Button = styled.button`
+//        /* Insert your favorite CSS code to style a button */
+//     `;
+//     // const FileUploader = props => {  // Create a reference to the hidden file input element
     
-    // };
+//     // };
     
-    return (
-        <form>
-            <div className="text-center" style ={{paddingTop: "25vh"}}>
-                <div id="logo-container">
-                        <div id="openVessel-logo">
-                            <img alt="default_user image" src={process.env.PUBLIC_URL + '/img/default_user.png'} style={{width: '20%', height: '20%'}}></img>
-                        </div>
-                    </div>
+//     return (
+//         <form>
+//             <div className="text-center" style ={{paddingTop: "25vh"}}>
+//                 <div id="logo-container">
+//                         <div id="openVessel-logo">
+//                             <img alt="default_user image" src={process.env.PUBLIC_URL + '/img/default_user.png'} style={{width: '20%', height: '20%'}}></img>
+//                         </div>
+//                     </div>
             
-                <Container>
-                    <Row className="justify-content-md-center">
-                        <Col xs lg="2">
-                            <input
-                                value={Username}
-                                onChange={e => setUsername(e.target.value)}
-                                placeholder="Username"
-                                type="text"
-                                name="setUsername"
-                                required
-                            />
-                        </Col>
+//                 <Container>
+//                     <Row className="justify-content-md-center">
+//                         <Col xs lg="2">
+//                             <input
+//                                 value={Username}
+//                                 onChange={e => setUsername(e.target.value)}
+//                                 placeholder="Username"
+//                                 type="text"
+//                                 name="setUsername"
+//                                 required
+//                             />
+//                         </Col>
     
-                        <Col xs lg="2">
+//                         <Col xs lg="2">
                         
-                            <input
-                                value={email}
-                                onChange={e => setEmail(e.target.value)}
-                                placeholder="Email address"
-                                type="email"
-                                name="email"
-                                required
-                            />
-                        </Col>
+//                             <input
+//                                 value={email}
+//                                 onChange={e => setEmail(e.target.value)}
+//                                 placeholder="Email address"
+//                                 type="email"
+//                                 name="email"
+//                                 required
+//                             />
+//                         </Col>
     
-                        <Col xs lg="2">
-                            <input
-                                value={password}
-                                onChange={e => setPassword(e.target.value)}
-                                placeholder="Password"
-                                type="password"
-                                name="password"
-                                required
-                            />
-                        </Col>
+//                         <Col xs lg="2">
+//                             <input
+//                                 value={password}
+//                                 onChange={e => setPassword(e.target.value)}
+//                                 placeholder="Password"
+//                                 type="password"
+//                                 name="password"
+//                                 required
+//                             />
+//                         </Col>
     
                         
-                    </Row> 
-                    <Button onClick={handleClick}>
-                            Upload a file
-                            </Button>
-                            <input
-                            type="file"
-                            ref={hiddenFileInput}
-                            onChange={handleChange}
-                            style={{display: 'none'}} 
-                        />
+//                     </Row> 
+//                     {/* <Button onClick={handleClick}>
+//                             Upload a file
+//                             </Button>
+//                             <input
+//                             type="file"
+//                             ref={hiddenFileInput}
+//                             onChange={handleChange}
+//                             style={{display: 'none'}} 
+//                         /> */}
     
-                </Container>
-                <button type="submit">Submit</button>
-            </div>
-        </form>
+//                     <div
+//                     style={{
+//                         display: "flex",
+//                         flexDirection: "column",
+//                         alignItems: "center",
+//                         justifyContent: "center"
+//                     }}
+//                     >
+//                     <input
+//                         type="file"
+//                         accept="image/*"
+//                         onChange={handleImageUpload}
+//                         ref={imageUploader}
+//                         style={{
+//                         display: "none"
+//                         }}
+//                     />
+//                     <div
+//                         style={{
+//                         height: "60px",
+//                         width: "60px",
+//                         border: "1px dashed black"
+//                         }}
+//                         onClick={() => imageUploader.current.click()}
+//                     >
+//                         <img
+//                         ref={uploadedImage}
+//                         style={{
+//                             width: "100%",
+//                             height: "100%",
+//                             position: "absolute"
+//                         }}
+//                         />
+//                     </div>
+//                     Click to upload Image
+//                     </div>
+
+
+//                 </Container>
+//                 <button type="submit">Submit</button>
+//             </div>
+//         </form>
     
-    );
-        };
-        export default Form;
+//         );
+//     };
+//         export default Form;
 
 
     // ----------------------------------------------------------------------
@@ -318,3 +390,148 @@ function Form(props) {
 
 // };
 // export default Form FileUploader;
+
+
+// ------------------------
+
+// import React, { useState } from 'react';
+// import ImageUploader from 'react-images-upload';
+// import Axios from 'axios';
+
+// // import './App.css';
+
+// const UploadComponent = props => (
+//     <form>
+//         <label>
+//             File Upload URL:
+//             <input id="urlInput" type="text" onChange={props.onUrlChange} value={props.url}></input>
+//         </label>
+//         <ImageUploader
+//             key="image-uploader"
+//             withIcon={true}
+//             singleImage={true}
+//             withPreview={true}
+//             label="Maximum size file: 5MB"
+//             buttonText="Choose an image"
+//             onChange={props.onImage}
+//             imgExtension={['.jpg', '.png', '.jpeg']}
+//             maxFileSize={5242880}
+//         ></ImageUploader>
+//     </form>
+// );
+
+// const App = () => {
+//     const [progress, setProgress] = useState('getUpload');
+//     const [url, setImageURL] = useState(undefined);
+//     const [errorMessage, setErrorMessage] = useState('');
+
+//     const onUrlChange = e => {
+//         setImageURL(e.target.value);
+//     };
+
+//     const onImage = async (failedImages, successImages) => {
+//         if (!url) {
+//             console.log('missing Url');
+//             setErrorMessage('missing a url to upload to');
+//             setProgress('uploadError');
+//             return;
+//         }
+
+//         setProgress('uploading');
+
+//         try {
+//             console.log('successImages', successImages);
+//             const parts = successImages[0].split(';');
+//             const mime = parts[0].split(':')[1];
+//             const name = parts[1].split('=')[1];
+//             const data = parts[2];
+//             const res = await Axios.post(url, { mime, name, image: data });
+
+//             setImageURL(res.data.imageURL);
+//             setProgress('uploaded');
+//         } catch (error) {
+//             console.log('error in upload', error);
+//             setErrorMessage(error.message);
+//             setProgress('uploadError');
+//         }
+//     };
+
+//     const content = () => {
+//         switch (progress) {
+//             case 'getUpload':
+//                 return <UploadComponent onUrlChange={onUrlChange} onImage={onImage} url={url} />;
+//             case 'uploading':
+//                 return <h2>Uploading....</h2>;
+//             case 'uploaded':
+//                 return <img src={url} alt="uploaded" />;
+//             case 'uploadError':
+//                 return (
+//                     <>
+//                         <div>Error message = {errorMessage}</div>
+//                         <UploadComponent onUrlChange={onUrlChange} onImage={onImage} url={url} />
+//                     </>
+//                 );
+//         }
+//     };
+
+//     return (
+//         <div className="App">
+//             <h1>Image Upload Website</h1>
+//             {content()}
+//         </div>
+//     );
+// };
+
+// export default App;
+
+// -------------
+
+
+
+// import React from 'react';
+// import ImageUploader from 'react-images-upload';
+
+// class App extends React.Component {
+
+//     constructor(props) {
+//         super(props);
+//         this.state = { pictures: [] };
+//         this.onDrop = this.onDrop.bind(this);
+//     }
+
+//     onDrop(picture) {
+//         this.setState({
+//             pictures: this.state.pictures.concat(picture),
+//         });
+//     }
+
+//     render() {
+//         return (
+//             <ImageUploader
+//                 withIcon={true}
+//                 buttonText='Choose images'
+//                 onChange={this.onDrop}
+//                 imgExtension={['.jpg', '.gif', '.png', '.gif']}
+//                 maxFileSize={5242880}
+//             />
+//         );
+//     }
+// }
+
+// ------------------------
+
+// import React from 'react';
+// import FileUpload from './components/FileUpload';
+// import './App.css';
+
+// const App = () => (
+//     <div className='container mt-4'>    
+//         <h4 className='display-4 text-center mb-4'>
+//             <i className='fab fa-react' /> React File Upload
+//         </h4>
+
+//         <FileUpload />
+//     </div>
+// );
+
+// export default App;
